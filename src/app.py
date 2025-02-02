2dce80190ee5300e63984d2de8bd94b47b5ecc1a
jonathanferreirar <jonathanferreirar@gmail.com>
2025-02-02 00:07:45 -0300

    :sparkles: feat: add cors config

--- src/app.py
+++ src/app.py
@@ -1,9 +1,12 @@
 from config import create_app
+from flask_cors import CORS
 from utils.extentions import bcrypt, jwt
 
 app = create_app()
+
 bcrypt.init_app(app)
 jwt.init_app(app)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 if __name__ == '__main__':
     app.run(debug=True, port=3001)
